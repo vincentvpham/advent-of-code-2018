f5420ba6096539e33f8ac4e3c9e8a8a1d54a70d6
Vincent Pham <vincentvpham@gmail.com>
2018-12-04 08:37:58 -0800

    find the first repeating frequency

--- 1/index.mjs
+++ 1/index.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import readline from 'readline';
 
-const getFrequency = () => {
+const calculateFrequency = () => {
 
   let frequency = 0;
 
@@ -18,4 +18,40 @@ const getFrequency = () => {
   });
 }
 
-getFrequency();
+const getRepeatedFrequency = () => {
+
+  const tracker = {};
+  let repeat;
+
+  const readFile = (frequency) => {
+
+    const rl = readline.createInterface({
+      input: fs.createReadStream('./data')
+    });
+
+    rl.on('line', line => {
+      frequency += Number.parseInt(line);
+      if (tracker[frequency] === true) {
+        // rl.close does not close the stream immediately
+        if (!repeat) {
+          repeat = frequency;
+        }
+        rl.close();
+      } else {
+        tracker[frequency] = true;
+      }
+    });
+
+    rl.on('close', () => {
+      if (!repeat) {
+        readFile(frequency);
+      } else {
+        console.log(repeat);
+      }
+    });
+  }
+
+  readFile(0);
+}
+
+getRepeatedFrequency();
